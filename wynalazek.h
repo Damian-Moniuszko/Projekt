a23fec62b2c3c4643e02cfade305c440a7b44b58
Damian Moniuszko <damianmoniuszko@MacBook-Air-Damian.local>
2026-01-07 14:53:14 +0100

    Dodano podstawowe biblioteki i typ wyliczeniowy StatusWynalazku

---
+++ wynalazek.h
@@ -0,0 +1,15 @@
+#ifdef WYNALEZK_H
+#define WYNALEZK_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef enum {
+    PROTOTYP,
+    W_TESTACH,
+    DOPUSZCZONY,
+    ZAKAZANY,
+    NIESTABILNY
+} StatusWynalazku;
+
+#endif
